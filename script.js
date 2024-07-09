3738f9bf14289a6091e0c245f581962040391a41
Soumya Mittal <soumyamittalrgh@gmail.com>
2024-07-09 18:30:19 +0530

    structure ready

---
+++ script.js
@@ -0,0 +1,54 @@
+let menuIcon =document.querySelector('#menu-icon');
+let navbar =document.querySelector('.navbar');
+
+menuIcon.onclick =() => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active')
+}
+
+
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll =() =>{
+    sections.forEach( sec =>{
+        let top=window.scrollY;
+        let offset= sec.offsetTop - 150;
+        let heigth =sec.offsetHeight;
+        let id =sec.getAttribute('id');
+
+        if(top >= offset && top < offset + heigth){
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[herf*=' + id + ']').classList.add('active');
+
+            });
+
+
+        };
+    });
+
+    let header=document.querySelector('header');
+    header.classList.toggle('sticky',window.scrolly >100);
+
+
+
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
+
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content,heading',{origin:'top'});
+
+ScrollReveal().reveal('.home-img,.mywork-box',{origin:'buttom'});
+
+ScrollReveal().reveal('.home-content h1, .about-img',{origin:'left'});
+
+ScrollReveal().reveal('.home-content p,.about-content',{origin:'right'});
+
+
