9c89e4ab7ec1067b204e82db105a0b8678a3261f
Soumya Mittal <soumyamittalrgh@gmail.com>
2024-07-11 14:57:44 +0530

    about section update

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 let menuIcon =document.querySelector('#menu-icon');
 let navbar =document.querySelector('.navbar');
 
+var tablinks =document.getElementsByClassName("tab-links");
+var tabcontents =document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab")
+}
+
+
+
 menuIcon.onclick =() => {
     menuIcon.classList.toggle('fa-xmark');
     navbar.classList.toggle('active')
